40f59aba8c17a8610e79a003a1b68593db458a0f
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-10-07 12:43:04 +0100

    Help.

--- Drums/script.js
+++ Drums/script.js
@@ -1,13 +1,13 @@
-const main = document.querySelector("main");
-let arr = ["ride", "crash", "floor-tom", "medium-tom", "high-tom", "hi-hat", "snare", "bass-drum"];
+// const main = document.querySelector("main");
+// let arr = ["ride", "crash", "floor-tom", "medium-tom", "high-tom", "hi-hat", "snare", "bass-drum"];
 
-for (let instrument of arr) {
-	let button = document.createElement("button");
-	button.innerHTML = instrument;
-	button.setAttribute("id", `${instrument}`);
-	main.appendChild(button);
-	button.addEventListener("click", () => {
-		let aud = new Audio(`./Assets/${button.innerHTML}.wav`);
-		aud.play();
-	})
-}
+// for (let instrument of arr) {
+// 	let button = document.createElement("button");
+// 	button.innerHTML = instrument;
+// 	button.setAttribute("id", `${instrument}`);
+// 	main.appendChild(button);
+// 	button.addEventListener("click", () => {
+// 		let aud = new Audio(`./Assets/${button.innerHTML}.wav`);
+// 		aud.play();
+// 	})
+// }
